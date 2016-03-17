bd0c7e39a1e59bc27201dfa65abab668011345b7
f100cleveland <dustin.rinne@gmail.com>
2016-03-17 16:47:00 -0500

    cpufreq: interactive: prevents the frequency to directly raise above the hispeed_freq from a lower frequency.
    When the load was below go_hispeed_load, there is a possibility that
    choose_freq() would return a frequency which would be higher than the
    hispeed_freq. According to the policy we should first jump to the
    hispeed_freq, stay there for above_hispeed_delay and then be allowed to
    raise higher than that.
    
    Added a check to prevent the frequency to be directly raised to
    something higher than the hispeed_freq.
    
    Change-Id: Icda5d848dd9beadcc18835082ddf269732c75bd0
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    (cherry picked from commit 4c7894cb01c7544225d5fcfb0fcb1d11fd2d1777) (reverted from commit e94f0f0a24fd3bcf00e812995e727928efbe742b)

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -474,10 +474,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 				new_freq = tunables->hispeed_freq;
 		}
 	} else {
-		new_freq = choose_freq(ppol, loadadjfreq);
-		if (new_freq > tunables->hispeed_freq &&
-				ppol->target_freq < tunables->hispeed_freq)
-			new_freq = tunables->hispeed_freq;
+		new_freq = choose_freq(pcpu, loadadjfreq);
 	}
 
 	if (cpu_load <= MAX_LOCAL_LOAD &&
